0dd7f63d20cf2701379f252ba3caef7ab5198ccc
Tylor Trub <tylortrub@gmail.com>
2025-11-24 18:40:38 -0500

    Nullable properties in UserAppSettings

--- Quantra.DAL/Data/Entities/ConfigurationEntities.cs
+++ Quantra.DAL/Data/Entities/ConfigurationEntities.cs
@@ -42,17 +42,17 @@ namespace Quantra.DAL.Data.Entities
 
         public int TabOrder { get; set; }
 
-        public string CardPositions { get; set; }
+        public string? CardPositions { get; set; }
 
-        public string ControlsConfig { get; set; }
+        public string? ControlsConfig { get; set; }
 
-        public string ToolsConfig { get; set; }
+        public string? ToolsConfig { get; set; }
 
         public int GridRows { get; set; } = 4;
 
         public int GridColumns { get; set; } = 4;
 
-        public string DataGridConfig { get; set; }
+        public string? DataGridConfig { get; set; }
     }
 
     /// <summary>
@@ -146,47 +146,6 @@ namespace Quantra.DAL.Data.Entities
 
         [MaxLength(200)]
         public string AlertEmail { get; set; } = "tylortrub@gmail.com";
-    }
-
-    /// <summary>
-    /// Entity representing a settings profile
-    /// </summary>
-    [Table("SettingsProfiles")]
-    public class SettingsProfile
-    {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; }
-
-        [MaxLength(500)]
-        public string Description { get; set; }
-
-        public bool IsDefault { get; set; }
-
-        public bool EnableApiModalChecks { get; set; }
-        public int ApiTimeoutSeconds { get; set; }
-        public int CacheDurationMinutes { get; set; }
-        public bool EnableHistoricalDataCache { get; set; }
-        public bool EnableDarkMode { get; set; }
-        public int ChartUpdateIntervalSeconds { get; set; }
-        public bool EnablePriceAlerts { get; set; }
-        public bool EnableTradeNotifications { get; set; }
-        public bool EnablePaperTrading { get; set; }
-
-        [MaxLength(50)]
-        public string RiskLevel { get; set; }
-
-        public int DefaultGridRows { get; set; }
-        public int DefaultGridColumns { get; set; }
-
-        [MaxLength(20)]
-        public string GridBorderColor { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-        public DateTime LastModified { get; set; }
     }
 
     /// <summary>
