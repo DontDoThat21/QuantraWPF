5b36fc0770fd8c23ec0a78273af2bf089e29c85a
Tylor Trub <tylortrub@gmail.com>
2025-12-02 09:15:15 -0500

    fixes

--- Quantra.Tests/CompressionTests.cs
+++ Quantra.Tests/CompressionTests.cs
@@ -76,7 +76,7 @@ namespace Quantra.Tests
 
             // Act
             string compressed = CompressionHelper.CompressString(input);
-            string decompressed = Quantra.CompressionHelper.DecompressString(compressed);
+            string decompressed = CompressionHelper.DecompressString(compressed);
 
             // Assert
             Assert.AreEqual(input, decompressed, "Empty string should be handled correctly");
